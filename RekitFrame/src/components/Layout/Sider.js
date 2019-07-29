cd09e0b7ff1095c26303942b21dc7dbb2a0e2069
chen <appleidofcl@163.com>
2019-07-29 15:32:07 +0800

    init

---
+++ RekitFrame/src/components/Layout/Sider.js
@@ -0,0 +1,67 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import { Icon, Layout } from 'antd'
+import { config } from '../../utils/config'
+import SiderMenu from './Menu'
+import styles from './Sider.less'
+
+class Sider extends PureComponent {
+  render() {
+    const {
+      menus,
+      theme,
+      isMobile,
+      collapsed,
+      onThemeChange,
+      onCollapseChange,
+    } = this.props
+
+    return (
+      <Layout.Sider
+        width={256}
+        theme={theme}
+        breakpoint="lg"
+        trigger={null}
+        collapsible
+        collapsed={collapsed}
+        onBreakpoint={isMobile ? null : onCollapseChange}
+        className="sider"
+      >
+        <div className="brand">
+          <div className="logo">
+            <img alt="logo" src={config.logoPath} />
+            {collapsed ? null : <h1>{config.siteName}</h1>}
+          </div>
+        </div>
+
+        <div className="menuContainer">
+          {/* <ScrollBar
+            options={{
+              // Disabled horizontal scrolling, https://github.com/utatti/perfect-scrollbar#options
+              suppressScrollX: true,
+            }}
+          > */}
+            <SiderMenu
+              menus={menus}
+              theme={theme}
+              isMobile={isMobile}
+              collapsed={collapsed}
+              onCollapseChange={onCollapseChange}
+            />
+          {/* </ScrollBar> */}
+        </div>
+      </Layout.Sider>
+    )
+  }
+}
+
+Sider.propTypes = {
+  menus: PropTypes.array,
+  theme: PropTypes.string,
+  isMobile: PropTypes.bool,
+  collapsed: PropTypes.bool,
+  onThemeChange: PropTypes.func,
+  onCollapseChange: PropTypes.func,
+}
+
+export default Sider
---
+++ RekitFrame/src/features/login/DefaultPage.js
@@ -0,0 +1,112 @@
+import React, { Component,Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as actions from './redux/actions';
+
+import { Button, Row, Form, Icon, Input } from 'antd'
+const FormItem = Form.Item
+
+class DefaultPage extends Component {
+  static propTypes = {
+    login: PropTypes.object.isRequired,
+    actions: PropTypes.object.isRequired,
+  };
+
+  handleOk = () => {
+    const { dispatch, form } = this.props
+    const { validateFieldsAndScroll } = form
+    validateFieldsAndScroll((errors, values) => {
+      if (errors) {
+        return
+      }
+      dispatch({ type: 'login/login', payload: values })
+    })
+  }
+
+  render() {
+    const { form } = this.props
+    const { getFieldDecorator } = form
+
+    let footerLinks = [
+      {
+        key: 'github',
+        title: <Icon type="github" />,
+        href: 'https://github.com/zuiidea/antd-admin',
+        blankTarget: true,
+      },
+    ]
+    
+    return (
+      <Fragment>
+        <div className="form">
+          <div className="logo">
+            <span>后台管理</span>
+          </div>
+          <form>
+            <FormItem hasFeedback>
+              {getFieldDecorator('username', {
+                rules: [
+                  {
+                    required: true,
+                  },
+                ],
+              })(
+                <Input
+                  onPressEnter={this.handleOk}
+                  placeholder="请输入用户名"
+                />
+              )}
+            </FormItem>
+            <FormItem hasFeedback>
+              {getFieldDecorator('password', {
+                rules: [
+                  {
+                    required: true,
+                  },
+                ],
+              })(
+                <Input
+                  type="password"
+                  onPressEnter={this.handleOk}
+                  placeholder="请输入密码"
+                />
+              )}
+            </FormItem>
+            <Row>
+              <Button
+                type="primary"
+                onClick={this.handleOk}
+              >
+                  登录
+              </Button>
+            </Row>
+          </form>
+        </div>
+        <div className="footer">
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+/* istanbul ignore next */
+function mapStateToProps(state) {
+  return {
+    login: state.login,
+  };
+}
+
+/* istanbul ignore next */
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators({ ...actions }, dispatch)
+  };
+}
+
+const WrappedNormalLoginForm = Form.create({ name: 'normal_login' })(DefaultPage);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(WrappedNormalLoginForm);
